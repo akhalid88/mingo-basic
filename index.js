05502789bf0bc83811ee33f54f4814b06c7321e3
Muhammad A Khalid <akhalid.code@gmail.com>
2021-06-23 16:00:19 -0700

    update board generation

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-const generateBoard = function (range, x, y) {
+const board = [];
+const callList = [];
+
+const generateBoard = function (dim, max) {
 	// var r = Math.floor(Math.random() * range) + 1;
-	const board = {};
-	const usedNums = {};
+	// const board = {};
+	let usedNums = {};
 
-	let i = 0;
-	while (i < x * y) {
-		let r = Math.floor(Math.random() * range) + 1;
-		if (!usedNums[r]) {
-			usedNums[r] = true;
-			board[i] = r;
-			i++
-		}
-	}
-
-	console.table(board);
+	for (let i = 0; i < dim; i++) {
+    let row = [];
+    for (let j = 0; j < dim; j++) {
+      row.push(getRandomInt(max, usedNums));
+    }
+    board.push(row);
+  }
+  // console.log(`This board has unique values in each cell: ${verifyBoard()}`);
 }
 
+function getRandomInt(max, target) {
+  let ranNum = Math.floor((Math.random() * max) + 1);
+  while (target[ranNum]) {
+    ranNum = Math.floor((Math.random() * max) + 1);
+  }
+  target[ranNum] = true;
+  return ranNum
+}
 // generateBoard(1000000, 1, 2);
-generateBoard(100, 10, 10);
+generateBoard(10, 100);
+
+
+console.table(board)
